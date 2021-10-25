12614281df898712d83bcdb0224082ce15bf2fc8
Scout McFall <scoutmknz@gmail.com>
2021-10-25 19:40:54 +0000

    got a working todo list

--- static/js/todolist.jsx
+++ static/js/todolist.jsx
@@ -4,21 +4,20 @@
 function ListItem(props) {
   //make a listitem object out of userInput
   // props.listItems.push(props);
-  // console.log(props.listItems)
+  console.log(props.listItems)
 
     const markDone = (evt) => {
       evt.preventDefault();
       //marks the done prop as true
       //eventually i will want to make this a toggle fxn
-      // console.log(props.listItems);
-      evt.target.value = 'good job!';
+      console.log(props.listItems);
+      evt.target.task = 'good job!';
       evt.target.className = 'true';
       console.log(evt.target.className);
     }
-  return (<div className = "todo">    
-    <p>
-      <button onClick = {markDone} value = {props.task} className = 'false'> {props.task}</button>
-    </p>
+  return (<div className = "list">    
+      <button onClick = {markDone} value = {props.task} className = "btn btn-outline-light"> {props.task}</button>
+    
   </div>
   );
 }
@@ -41,17 +40,23 @@ function ToDoList() {
 
   //for some reason the last two tasks from the list 
   //items array are being cleared instead of the completed task
-  const doneItems = [];
-  const clearDone = (evt) =>{
-    for (let item of listItems) {
-        if (item.className == 'true');
-          // console.log (item);
-          doneItems.push({item});
-          listItems.pop({item});
-          //i think i need to map the list in order to remove items from it?
-          console.log(listItems);
-    }
-  }
+  // const doneItems = [];
+  // const clearDone = (evt) => {
+  //   //use filter to return a new and filtered list
+  //   const newList = listItems.filter((item) => {
+  //     item.className != 'false'});
+
+  //   setListItems(newList);
+
+
+    // for (let item of listItems) {
+    //     if (item.className == 'true');
+    //       // console.log (item);
+    //       doneItems.push({item});
+    //       // listItems.pop({item});
+    //       console.log(listItems);
+    // }
+  // }
 
   //another big arrow function
   const handleSubmit = (evt) => {
@@ -65,30 +70,29 @@ function ToDoList() {
   }
 
 //return a react fragment containing ListItem in the return statement
-  return <React.Fragment>
-    {/* call clear done each time I render? */}
+    return <React.Fragment>
 
-    {/* //form getting userInput */}
-      <form onSubmit = {handleSubmit}>
-        <input type = "text" value = {userInput} placeholder = "do more" onChange = {handleChange} />
-        <button type = "submit">add item</button>
-      </form>
-    {/* //up to date list */}
-      <div className = "todo">To-Do List:
-      {/* user .map method on the list, copies each element in a list but can also pass it a callback fxn
-      that tells it what each copy should look like so you can create elements */}
+      {/* //form getting userInput */}
+        <form onSubmit = {handleSubmit} className = "form-control form-control-lg">
+          <input type = "text" value = {userInput} placeholder = "do more" onChange = {handleChange} />
+          <button type = "submit" className = "btn btn-outline-light">add item</button>
+        </form>
+      {/* //up to date list */}
+        <div className = "todo"><h1>To-Do List:</h1>
+        {/* user .map method on the list, copies each element in a list but can also pass it a callback fxn
+        that tells it what each copy should look like so you can create elements */}
 
-    <ul>
-      {listItems.map((listItem, index) => (
-        <ListItem key = {index}task = {listItem} listItems = {listItems} className = 'false'/> //can add more properties to it if i need to
-      ))}
-    </ul>
-    <p><button onClick = {clearDone} value = 'clear completed'> clear completed </button></p>
-
-      </div>
-       
-      </React.Fragment>
-  }
+      <ul className = 'list'>
+        {listItems.map((listItem, index) => (
+          <ListItem key = {index} task = {listItem} listItems = {listItems} className = 'false'/> 
+          //can add more properties to it if i need to
+        ))}
+      </ul>
+      {/* <p><button onClick = {clearDone} value = 'clear completed' key ={listItem.key} > clear completed </button></p> */}
+        </div>
+        
+        </React.Fragment>
+}
 
 ReactDOM.render(<ToDoList />, document.getElementById('todolist'));
 
